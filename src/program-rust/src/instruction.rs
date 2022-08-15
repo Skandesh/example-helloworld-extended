b902753cf8ad44aa3e559fe2efa5efc0ec51f650
Skandesh <42321593+Skandesh@users.noreply.github.com>
2022-08-15 11:27:58 +0530

    alternative solution -- error propogation
    instead of match, using '?' to do the same process

--- src/program-rust/src/instruction.rs
+++ src/program-rust/src/instruction.rs
@@ -12,23 +12,18 @@ impl HelloInstruction {
     pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
         let (&tag, rest) = input.split_first().ok_or(ProgramError::InvalidInstructionData)?;
 
-        match tag {
+        match  tag {
             0 => return Ok(HelloInstruction::Increment),
             1 => return Ok(HelloInstruction::Decrement),
             2 => {
-                if rest.len()!=4 {
+                if rest.len()!= 4 {
                     return Err(ProgramError::InvalidInstructionData);
-                }
-                let val: Result<[u8; 4], _> = rest[..4].try_into();
-                match val {
-                    Ok(i) => {
-                        return Ok(HelloInstruction::Set(u32::from_le_bytes(i)))
-                    },
-                    _ => return Err(ProgramError::InvalidInstructionData)
-            }
-    
+                } 
+                let val:Result<[u8; 4],_> = rest[..4].try_into();
+                return  HelloInstruction::Set(u32::from_le_bytes(val))?;
+              
             },
-            _ => return Err(ProgramError::InvalidInstructionData)
+            _=>     return Err(ProgramError::InvalidInstructionData)
         }
 
     
